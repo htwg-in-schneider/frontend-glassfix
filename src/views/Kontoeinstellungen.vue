1f91b7e7b3750981538e44c9d17b736ff40eb794
Juan Cerda <ju451cer@htwg-konstanz.de>
2026-06-25 07:49:42 +0200

    abmelden route

--- src/views/Kontoeinstellungen.vue
+++ src/views/Kontoeinstellungen.vue
@@ -15,7 +15,7 @@ const router = useRouter();
 const handleLogout = () => {
   logout({
     logoutParams: {
-      returnTo: window.location.origin
+      returnTo: window.location.origin + '/frontend-glassfix/'
     }
   });
 }
